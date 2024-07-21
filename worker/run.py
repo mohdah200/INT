b65af18c915ca4bdc56c03b892ba8f60c6f10b76
Muhammed Daham <35640445+mohdah200@users.noreply.github.com>
2024-07-21 17:24:54 +0800

    Update run.py

--- worker/run.py
+++ worker/run.py
@@ -2,27 +2,21 @@ import importlib
 import os
 import sys
 
-
 def get_curr_working_dir():
     curr_working_dir = os.getcwd()
     return curr_working_dir
 
-
 def run():
     current_working_directory = get_curr_working_dir()
-    sys.path.append("{}".format(current_working_directory))
-    sys.path.append("{}/challenge_data/challenge_1".format(current_working_directory))
+    sys.path.append(current_working_directory)
+    sys.path.append(f"{current_working_directory}/challenge_data/challenge_1")
 
     challenge_id = 1
-    challenge_phase = "test"  # Add the challenge phase codename to be tested
-    annotation_file_path = "{}/annotations/test_annotations_testsplit.json".format(
-        current_working_directory
-    )  # Add the test annotation file path
-    user_submission_file_path = "{}/submission.json".format(
-        current_working_directory
-    )  # Add the sample submission file path
+    challenge_phase = "single_phase"  # Update with your actual phase codename
+    annotation_file_path = f"{current_working_directory}/annotations/ground_truth.csv"  # Path to your ground truth labels
+    user_submission_file_path = f"{current_working_directory}/submission.csv"  # Path to the sample submission file
 
-    CHALLENGE_IMPORT_STRING = "challenge_data.challenge_1"
+    CHALLENGE_IMPORT_STRING = "challenge_data.challenge_1.main"
     challenge_module = importlib.import_module(CHALLENGE_IMPORT_STRING)
 
     EVALUATION_SCRIPTS = {}
@@ -32,7 +26,7 @@ def run():
         "status": u"running",
         "when_made_public": None,
         "participant_team": 5,
-        "input_file": "https://abc.xyz/path/to/submission/file.json",
+        "input_file": "https://abc.xyz/path/to/submission/file.csv",
         "execution_time": u"123",
         "publication_url": u"ABC",
         "challenge_phase": 1,
@@ -48,14 +42,14 @@ def run():
         "id": 123,
         "submitted_at": u"2017-03-20T19:22:03.880652Z",
     }
-    EVALUATION_SCRIPTS[challenge_id].evaluate(
+    result = EVALUATION_SCRIPTS[challenge_id].evaluate(
         annotation_file_path,
         user_submission_file_path,
         challenge_phase,
         submission_metadata=submission_metadata,
     )
     print("Evaluated Successfully!")
-
+    print(result)
 
 if __name__ == "__main__":
     run()
