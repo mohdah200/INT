0b9fd38f4d3eecbc8a6f49c898175d27ced85c16
Muhammed Daham <35640445+mohdah200@users.noreply.github.com>
2024-07-22 10:49:55 +0800

    Update __init__.py

--- challenge_data/challenge_1/__init__.py
+++ challenge_data/challenge_1/__init__.py
@@ -1,1 +1,54 @@
+"""
+# Q. How to install custom python pip packages?
+
+# A. Uncomment the below code to install the custom python packages.
+
+import os
+
+
+from pathlib import Path
+from challenge_1.main import evaluate_accuracy
+from challenge_2.main import evaluate_bleu_cider
+
+
+
+
+def install_local_package(folder_name):
+    # Install a local python package
+
+    # Args:
+    #     folder_name ([str]): name of the folder placed in evaluation_script/
+    
+    subprocess.check_output(
+    [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
+    ]
+)
+
+install("shapely==1.7.1")
+install("requests==2.25.1")
+
+install_local_package("package_folder_name")
+
+"""
+import os
+import subprocess
+import sys
+from pathlib import Path
+def install(package):
+    # Install a pip python package
+
+    # Args:
+    #     package ([str]): Package name with version
+    
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install ("numpy")
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 from .main import evaluate
+
+
